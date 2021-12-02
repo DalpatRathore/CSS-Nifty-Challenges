11a2bf545604ae7556e0dbac1993a6341fa85c7b
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2021-12-02 21:33:09 +0530

    Contributed Menu Icon

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Daps");
+const menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+});
+
+const menuIcon2 = document.querySelector(".menu-icon2");
+
+menuIcon2.addEventListener("click", () => {
+  menuIcon2.classList.toggle("active2");
+});
